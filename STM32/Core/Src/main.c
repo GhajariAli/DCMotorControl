323deae60d3cb5ad2bd311ddb3f9b47cff401de9
DESKTOP-6PH1888\User <Ali.Flnjd@gmail.com>
2023-12-27 01:50:24 -0500

    adds enable circuit

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -260,6 +260,7 @@ int main(void)
 	 		  if(PID.target==-250) PID.target=250;
 	 		  else if (PID.target==250) PID.target=-250;
 	 		  speedUpdateTime=HAL_GetTick();
+	 		  HAL_GPIO_TogglePin(Motor_Enable_GPIO_Port, Motor_Enable_Pin);
 		  }
 		  ProcessValue= TestEncoder.SpeedRPM;
 		  PreviousOperationMode=OperationMode;
@@ -274,8 +275,6 @@ int main(void)
 		  SystemTime=HAL_GetTick();
 		  updatePID(&PID, ProcessValue);
 		  __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,PID.output);
-		  HAL_GPIO_WritePin(Motor_Enable_GPIO_Port, Motor_Enable_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(Motor_Enable_GPIO_Port, Motor_Enable_Pin, GPIO_PIN_SET);
 	  }
 	  if (HAL_GetTick()-messageUpdateTime>=50){
 		  char message[100];
